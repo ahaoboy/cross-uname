128ec3554ca919e8657f145b20a41661f439d8ac
ahaoboy <504595380@qq.com>
2024-06-19 15:08:41 +0800

    init

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,4 @@
 use std::io;
-
-#[cfg(not(windows))]
-mod unix;
-
-#[cfg(windows)]
-mod win;
-
 #[derive(Debug, Clone)]
 pub struct Info {
     pub sysname: String,
@@ -17,4 +10,31 @@ pub struct Info {
 
 pub fn uname() -> io::Result<Info> {
     Info::new()
+}
+
+impl Info {
+    pub fn new() -> io::Result<Self> {
+        fn exec(arg: &str) -> String {
+            let out = std::process::Command::new("uname")
+                .arg(arg)
+                .output()
+                .unwrap()
+                .stdout;
+            String::from_utf8(out).unwrap().trim().to_string()
+        }
+
+        let sysname = exec("-s");
+        let nodename = exec("-n");
+        let release = exec("-r");
+        let version = exec("-v");
+        let machine = exec("-m");
+
+        Ok(Info {
+            sysname,
+            nodename,
+            release,
+            version,
+            machine,
+        })
+    }
 }
